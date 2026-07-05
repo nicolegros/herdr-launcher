9e6f3b48350308edd13b3b1f645db9ee4848c882
Nicolas Legros <nicolaslegros98@gmail.com>
2026-07-05 12:57:26 -0400

    test: add tests for config expansion and directory scanning

---
+++ config_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestExpandPath(t *testing.T) {
+	home, _ := os.UserHomeDir()
+
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{"~", home},
+		{"~/Developer", filepath.Join(home, "Developer")},
+		{"/absolute/path", "/absolute/path"},
+		{"relative/path", "relative/path"},
+	}
+
+	for _, tt := range tests {
+		got := expandPath(tt.input)
+		if got != tt.want {
+			t.Errorf("expandPath(%q) = %q, want %q", tt.input, got, tt.want)
+		}
+	}
+}
